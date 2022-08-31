05fcea4ac1e54dbddf58a9a88dad4e4dc9aeaee9
RISKI <Riski180722@gmail.com>
2022-08-31 15:23:28 +0700

    Mengumpukan tugas C++ materi fungsi if else tunggal

---
+++ Tugas c++ materi fungsi IF ELSE tunggal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int X = 5;
+    int Z = 20;                                                                                  
+    int W;
+    int Y;
+
+    W = X + Z;
+    Y = X - Z;
+
+
+    if(X>=Z){
+        cout<< W;
+    }
+    else{
+        cout<< Y;
+    }
+    return 0;
+}
